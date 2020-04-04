2e6ea5754ae566934a83a40f4328443de04e7cd1
Patrícia Silva <prrs89@gmail.com>
2020-04-04 15:41:40 +0100

    webpack entry

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const webpack = require('webpack');
+
+/*
+default location for webpack entry point: './src/index.js'
+that file doesn't exist because of express (different file structure)
+*/
+
+module.exports = {
+  entry: './src/client/index.js',
+};
