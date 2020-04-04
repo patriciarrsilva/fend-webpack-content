07a9a2809620df536a6cb9d0187f363a73bbaffe
Patrícia Silva <prrs89@gmail.com>
2020-04-04 19:37:41 +0100

    add file loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
+import img from './img/favicon.png';
 
 console.log(checkForName);
 alert('I exist!');
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
 };
